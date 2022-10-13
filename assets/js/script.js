9b2bb49aadd73d3cd3115620d41ca72b98bf343b
jonpfluger <jonpfluger718@gmail.com>
2022-10-12 20:13:41 -0500

    local storage, unfinished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,44 @@
-var celebrityAPIKey = "GdDbXMeZU/1Ga5Z5HB4LVg==jEW7qzJCa2mUybyw"
-var astrologyAPIKey = "8ee4ec9d42msha4833b4bc3bb1b2p1bb387jsne21e89998379"
+var celebrityAPIKey = "GdDbXMeZU/1Ga5Z5HB4LVg==jEW7qzJCa2mUybyw";
+var astrologyAPIKey = "8ee4ec9d42msha4833b4bc3bb1b2p1bb387jsne21e89998379";
 
-var searchBar = $('#search-bar')
-var searchBtn = $('#search-btn')
-var resultsSection = $('#results-section')
-var pastSearches = $('#past-searches')
+var searchBar = $('#search-bar');
+var searchBtn = $('#search-btn');
+var resultsSection = $('#results-section');
+var pastSearches = $('#past-searches');
 
+var userInputArr = [];
+
+function getLocalStorage() {
+  var lsData= localStorage.getItem("userInputArr");
+  var lsDataParsed = JSON.parse(lsData);
+
+  userInputArr = lsDataParsed;
+  console.log(lsDataParsed)
+  
+
+  // // undefined
+  // if (localStorage.inputValues === undefined) {
+  //   console.log('undefined')
+
+  // // returns true if the value is 0, "", or null
+  // } else if (!localStorage.inputValues === true) {
+  //   console.log('0, "", or null')
+
+  // // returns true if its anything but
+  // } else {
+  //   console.log("anything but")
+    
+
+
+  // }
+}
+
+getLocalStorage()
 
 function celebritySearch() {
-  var userInput = searchBar.val()
-  var userInputLowerCase = userInput.toLowerCase()
-  var userInputTrimmed = userInputLowerCase.trim()
+  var userInput = searchBar.val();
+  var userInputLowerCase = userInput.toLowerCase();
+  var userInputTrimmed = userInputLowerCase.trim();
   $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/celebrity?name=' + userInputTrimmed,
@@ -19,7 +47,7 @@ function celebritySearch() {
     success: function(result) {
       if (result.length === 0) {
         // initialize modal element
-        var modalEl = document.createElement('div')
+        var modalEl = document.createElement('div');
         modalEl.classList.add("modal")
         modalEl.textContent = "Invalid search. Please try again. Click anywhere to continue."
 
@@ -30,15 +58,15 @@ function celebritySearch() {
         searchBar.val("")
       } else {
         console.log(result);
-        var cardEl = $('<div>').addClass('card')
-        var nameEl = $('<h2>').text(result[0].name)
-        var birthdayEl = $('<p>').text("Birthday: " + result[0].birthday)
+        var cardEl = $('<div>').addClass('card');
+        var nameEl = $('<h2>').text(result[0].name);
+        var birthdayEl = $('<p>').text("Birthday: " + result[0].birthday);
         cardEl.append(nameEl, birthdayEl)
 
         // occupations
         var occupationArr = result[0].occupation
         for (let i = 0; i < occupationArr.length; i++) {
-          var occupationEl = $('<p>').text(occupationArr[i])
+          var occupationEl = $('<p>').text(occupationArr[i]);
           cardEl.append(occupationEl)
         }
 
@@ -51,7 +79,7 @@ function celebritySearch() {
     error: function ajaxError(jqXHR) {
       console.error('Error: ', jqXHR.responseText);
       // initialize modal element
-      var modalEl = document.createElement('div')
+      var modalEl = document.createElement('div');
       modalEl.classList.add("modal")
       modalEl.textContent = "Invalid search. Please try again. Click anywhere to continue."
 
@@ -66,9 +94,9 @@ function celebritySearch() {
 
 
 function astrologySearch() {
-  var userInput = searchBar.val()
-  var userInputLowerCase = userInput.toLowerCase()
-  var userInputTrimmed = userInputLowerCase.trim()
+  var userInput = searchBar.val();
+  var userInputLowerCase = userInput.toLowerCase();
+  var userInputTrimmed = userInputLowerCase.trim();
   const settings = {
     "async": true,
     "crossDomain": true,
@@ -82,11 +110,11 @@ function astrologySearch() {
   
   $.ajax(settings).done(function (response) {
     console.log(response);
-    var cardEl = $('<div>').addClass('card')
-    var signEl = $('<h2>').text(userInputTrimmed)
-    var dateRangeEl = $('<p>').text("Date Range: " + response.date_range)
-    var descriptionEl = $('<p>').text("Daily horoscope: " + response.description)
-    var luckyTimeEl = $('<p>').text("Lucky time: " + response.lucky_time)
+    var cardEl = $('<div>').addClass('card');
+    var signEl = $('<h2>').text(userInputTrimmed);
+    var dateRangeEl = $('<p>').text("Date Range: " + response.date_range);
+    var descriptionEl = $('<p>').text("Daily horoscope: " + response.description);
+    var luckyTimeEl = $('<p>').text("Lucky time: " + response.lucky_time);
     cardEl.append(signEl, dateRangeEl, descriptionEl, luckyTimeEl)
     resultsSection.append(cardEl)
 
@@ -99,9 +127,13 @@ function astrologySearch() {
 function searchFunction(event) {
   event.preventDefault()
 
-  var userInput = searchBar.val()
-  var userInputLowerCase = userInput.toLowerCase()
-  var userInputTrimmed = userInputLowerCase.trim()
+  var userInput = searchBar.val();
+  var userInputLowerCase = userInput.toLowerCase();
+  var userInputTrimmed = userInputLowerCase.trim();
+
+  userInputArr.push(userInputTrimmed);
+  localStorage.setItem("userInputArr", JSON.stringify(userInputArr))
+  console.log(localStorage.userInputArr)
 
   if (userInputTrimmed === "capricorn" || userInputTrimmed === "libra" || userInputTrimmed === "aries" || userInputTrimmed === "cancer" || userInputTrimmed === "taurus" || userInputTrimmed === "gemini" || userInputTrimmed === "leo" || userInputTrimmed === "virgo" || userInputTrimmed === "scorpio" || userInputTrimmed === "pisces" || userInputTrimmed === "aquarius" || userInputTrimmed === "sagittarius") {
     astrologySearch()
@@ -113,84 +145,3 @@ function searchFunction(event) {
 
 // click listener to search button
 searchBtn.on('click', searchFunction)
-
-
-
-
-
-/*
-
--------CELEBRITY---------
-
-CODE SNIPPET:
-
-var name = 'Michael Jordan'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/celebrity?name=' + name,
-    headers: { 'X-Api-Key': celebrityAPIKey},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
-
-
-EXAMPLE RESPONSE:
-
-[
-  {
-    "name": "michael jordan",
-    "net_worth": 2200000000,
-    "gender": "male",
-    "nationality": "us",
-    "occupation": [
-      "basketball_player",
-      "athlete",
-      "spokesperson",
-      "entrepreneur",
-      "actor"
-    ],
-    "height": 1.98,
-    "birthday": "1963-02-17",
-  }
-]
-
-
-
-------ASTROLOGY--------
-
-CODE SNIPPET: 
-
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=aquarius&day=today",
-	"method": "POST",
-	"headers": {
-		"X-RapidAPI-Key": astrologyAPIKey,
-		"X-RapidAPI-Host": "sameer-kumar-aztro-v1.p.rapidapi.com"
-	}
-};
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
-
-
-EXAMPLE RESPONSE: 
-
-color:"Blue"
-compatibility:"Taurus"
-current_date:"September 24, 2020"
-date_range:"Jan 20 - Feb 18"
-description:"Your busy lifestyle has been fun, but today it's time to slow down and just relax."
-lucky_number:"93"
-lucky_time:"6pm"
-mood:"Relieved"
-
-
-*/
